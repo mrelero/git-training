512dfb94a6b714e9d96ed57c5ce76a09e1b9946f
Matheus Raphael Elero <matheus.elero@setis>
2022-09-20 11:37:10 -0300

    Uptd

--- subtreeTest/main.c
+++ subtreeTest/main.c
@@ -3,10 +3,10 @@
 
 int main(void){
 
-    printf("Oi!?");
-    printf("Hello!?");
-    printf("Hola!?");
-    printf("Hei!?");
+    printf("Oi!");
+    printf("Hello!");
+    printf("Hola!");
+    printf("Hei!");
 
     printf("Oi Mundo!");
     return 1;
