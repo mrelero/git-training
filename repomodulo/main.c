3dc7de57e48a1cb54bdb8fe2a9c5c4c5c5eb83f5
Matheus Raphael Elero <matheus.elero@setis>
2022-09-21 08:28:04 -0300

    git subrepo clone --force --branch=newbranch https://github.com/mrelero/subtree-test-study repomodulo
    subrepo:
      subdir:   "repomodulo"
      merged:   "d2aaff7"
    upstream:
      origin:   "https://github.com/mrelero/subtree-test-study"
      branch:   "newbranch"
      commit:   "d2aaff7"
    git-subrepo:
      version:  "0.4.3"
      origin:   "???"
      commit:   "???"

---
+++ repomodulo/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void){
+
+    printf("Oi");
+    printf("Hello");
+    printf("Hola");
+    printf("Hei");
+
+    printf("Oi Mundo!");
+
+    printf("Hello World!");
+
+    printf("Alo Alo!");
+
+    return 3;
+}
